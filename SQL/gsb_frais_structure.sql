5f2d574c0162fcc122b403121df3145225619375
aladore <cyril.napolitano42@gmail.com>
2014-10-03 11:41:11 +0200

    nouvelle table AffectationVehicule

--- SQL/gsb_frais_structure.sql
+++ SQL/gsb_frais_structure.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
+-- version 4.1.7
 -- http://www.phpmyadmin.net
 --
--- Serveur: localhost
--- Généré le : Lun 04 Juillet 2011 à 14:08
--- Version du serveur: 5.5.8
--- Version de PHP: 5.3.5
+-- Host: localhost
+-- Generation Time: Oct 03, 2014 at 11:36 AM
+-- Server version: 5.1.71-community-log
+-- PHP Version: 5.3.10
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -15,59 +17,57 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `gsb_frais`
+-- Database: `my_sionapolitano`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `FraisForfait`
+-- Table structure for table `AffectationVehicule`
 --
 
-CREATE TABLE IF NOT EXISTS `FraisForfait` (
-  `id` char(3) NOT NULL,
-  `libelle` char(20) DEFAULT NULL,
-  `montant` decimal(5,2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `AffectationVehicule` (
+  `idVisiteur` varchar(6) NOT NULL,
+  `mois` date NOT NULL,
+  `ImmatVehicule` varchar(11) NOT NULL,
+  PRIMARY KEY (`idVisiteur`,`mois`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `AffectationVehicule`
+--
+
+INSERT INTO `AffectationVehicule` (`idVisiteur`, `mois`, `ImmatVehicule`) VALUES
+('a131', '2014-11-13', 'CY-111-LL'),
+('a131', '2014-10-01', 'CZ-501-SR'),
+('cn20', '2015-06-11', 'GE-999-FF');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Etat`
+-- Table structure for table `Etat`
 --
 
 CREATE TABLE IF NOT EXISTS `Etat` (
   `id` char(2) NOT NULL,
   `libelle` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Etat`
+--
+
+INSERT INTO `Etat` (`id`, `libelle`) VALUES
+('RB', 'Remboursée'),
+('CL', 'Saisie clôturée'),
+('CR', 'Fiche créée, saisie en cours'),
+('VA', 'Validée et mise en paiement');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Visiteur`
---
-
-CREATE TABLE IF NOT EXISTS `Visiteur` (
-  `id` char(4) NOT NULL,
-  `nom` char(30) DEFAULT NULL,
-  `prenom` char(30)  DEFAULT NULL, 
-  `login` char(20) DEFAULT NULL,
-  `mdp` char(20) DEFAULT NULL,
-  `adresse` char(30) DEFAULT NULL,
-  `cp` char(5) DEFAULT NULL,
-  `ville` char(30) DEFAULT NULL,
-  `dateEmbauche` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
-
-
--- --------------------------------------------------------
-
---
--- Structure de la table `FicheFrais`
+-- Table structure for table `FicheFrais`
 --
 
 CREATE TABLE IF NOT EXISTS `FicheFrais` (
@@ -78,15 +78,44 @@ CREATE TABLE IF NOT EXISTS `FicheFrais` (
   `dateModif` date DEFAULT NULL,
   `idEtat` char(2) DEFAULT 'CR',
   PRIMARY KEY (`idVisiteur`,`mois`),
-  FOREIGN KEY (`idEtat`) REFERENCES Etat(`id`),
-  FOREIGN KEY (`idVisiteur`) REFERENCES Visiteur(`id`)
-) ENGINE=InnoDB;
+  KEY `idEtat` (`idEtat`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `FicheFrais`
+--
+
+INSERT INTO `FicheFrais` (`idVisiteur`, `mois`, `nbJustificatifs`, `montantValide`, `dateModif`, `idEtat`) VALUES
+('cn20', '09', 0, '0.00', '2014-10-01', 'CR'),
+('cn20', '10', 0, '0.00', '2014-10-22', 'CR');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `LigneFraisForfait`
+-- Table structure for table `FraisForfait`
+--
+
+CREATE TABLE IF NOT EXISTS `FraisForfait` (
+  `id` char(3) NOT NULL,
+  `libelle` char(20) DEFAULT NULL,
+  `montant` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `FraisForfait`
+--
+
+INSERT INTO `FraisForfait` (`id`, `libelle`, `montant`) VALUES
+('ETP', 'Forfait Etape', '110.00'),
+('KM', 'Frais Kilométrique', '0.62'),
+('NUI', 'Nuitée Hôtel', '80.00'),
+('REP', 'Repas Restaurant', '25.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `LigneFraisForfait`
 --
 
 CREATE TABLE IF NOT EXISTS `LigneFraisForfait` (
@@ -95,23 +124,92 @@ CREATE TABLE IF NOT EXISTS `LigneFraisForfait` (
   `idFraisForfait` char(3) NOT NULL,
   `quantite` int(11) DEFAULT NULL,
   PRIMARY KEY (`idVisiteur`,`mois`,`idFraisForfait`),
-  FOREIGN KEY (`idVisiteur`, `mois`) REFERENCES FicheFrais(`idVisiteur`, `mois`),
-  FOREIGN KEY (`idFraisForfait`) REFERENCES FraisForfait(`id`)
-) ENGINE=InnoDB;
+  KEY `idFraisForfait` (`idFraisForfait`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `LigneFraisForfait`
+--
+
+INSERT INTO `LigneFraisForfait` (`idVisiteur`, `mois`, `idFraisForfait`, `quantite`) VALUES
+('cn20', '09', 'ETP', 0),
+('cn20', '09', 'KM', 0),
+('cn20', '09', 'NUI', 0),
+('cn20', '09', 'REP', 0);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `LigneFraisHorsForfait`
+-- Table structure for table `LigneFraisHorsForfait`
 --
 
 CREATE TABLE IF NOT EXISTS `LigneFraisHorsForfait` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `idVisiteur` char(4) NOT NULL,
   `mois` char(6) NOT NULL,
   `libelle` varchar(100) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `montant` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (`idVisiteur`, `mois`) REFERENCES FicheFrais(`idVisiteur`, `mois`)
-) ENGINE=InnoDB;
+  PRIMARY KEY (`id`),
+  KEY `idVisiteur` (`idVisiteur`,`mois`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Visiteur`
+--
+
+CREATE TABLE IF NOT EXISTS `Visiteur` (
+  `id` char(4) NOT NULL,
+  `nom` char(30) DEFAULT NULL,
+  `prenom` char(30) DEFAULT NULL,
+  `login` char(20) DEFAULT NULL,
+  `mdp` char(20) DEFAULT NULL,
+  `adresse` char(30) DEFAULT NULL,
+  `cp` char(5) DEFAULT NULL,
+  `ville` char(30) DEFAULT NULL,
+  `dateEmbauche` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Visiteur`
+--
+
+INSERT INTO `Visiteur` (`id`, `nom`, `prenom`, `login`, `mdp`, `adresse`, `cp`, `ville`, `dateEmbauche`) VALUES
+('a131', 'Villechalane', 'Louis', 'lvillachane', 'jux7g', '8 rue des Charmes', '46000', 'Cahors', '2005-12-21'),
+('a17', 'Andre', 'David', 'dandre', 'oppg5', '1 rue Petit', '46200', 'Lalbenque', '1998-11-23'),
+('a55', 'Bedos', 'Christian', 'cbedos', 'toolsun', '1 rue Peranud', '46250', 'Montcuq', '1995-01-12'),
+('a93', 'Tusseau', 'Louis', 'ltusseau', 'ktp3s', '22 rue des Ternes', '46123', 'Gramat', '2000-05-01'),
+('b13', 'Bentot', 'Pascal', 'pbentot', 'doyw1', '11 allée des Cerises', '46512', 'Bessines', '1992-07-09'),
+('b16', 'Bioret', 'Luc', 'lbioret', 'hrjfs', '1 Avenue gambetta', '46000', 'Cahors', '1998-05-11'),
+('b19', 'Bunisset', 'Francis', 'fbunisset', '4vbnd', '10 rue des Perles', '93100', 'Montreuil', '1987-10-21'),
+('b25', 'Bunisset', 'Denise', 'dbunisset', 's1y1r', '23 rue Manin', '75019', 'paris', '2010-12-05'),
+('b28', 'Cacheux', 'Bernard', 'bcacheux', 'uf7r3', '114 rue Blanche', '75017', 'Paris', '2009-11-12'),
+('b34', 'Cadic', 'Eric', 'ecadic', '6u8dc', '123 avenue de la République', '75011', 'Paris', '2008-09-23'),
+('b4', 'Charoze', 'Catherine', 'ccharoze', 'u817o', '100 rue Petit', '75019', 'Paris', '2005-11-12'),
+('b50', 'Clepkens', 'Christophe', 'cclepkens', 'bw1us', '12 allée des Anges', '93230', 'Romainville', '2003-08-11'),
+('b59', 'Cottin', 'Vincenne', 'vcottin', '2hoh9', '36 rue Des Roches', '93100', 'Monteuil', '2001-11-18'),
+('c14', 'Daburon', 'François', 'fdaburon', '7oqpv', '13 rue de Chanzy', '94000', 'Créteil', '2002-02-11'),
+('c3', 'De', 'Philippe', 'pde', 'gk9kx', '13 rue Barthes', '94000', 'Créteil', '2010-12-14'),
+('c54', 'Debelle', 'Michel', 'mdebelle', 'od5rt', '181 avenue Barbusse', '93210', 'Rosny', '2006-11-23'),
+('d13', 'Debelle', 'Jeanne', 'jdebelle', 'nvwqq', '134 allée des Joncs', '44000', 'Nantes', '2000-05-11'),
+('d51', 'Debroise', 'Michel', 'mdebroise', 'sghkb', '2 Bld Jourdain', '44000', 'Nantes', '2001-04-17'),
+('e22', 'Desmarquest', 'Nathalie', 'ndesmarquest', 'f1fob', '14 Place d Arc', '45000', 'Orléans', '2005-11-12'),
+('e24', 'Desnost', 'Pierre', 'pdesnost', '4k2o5', '16 avenue des Cèdres', '23200', 'Guéret', '2001-02-05'),
+('e39', 'Dudouit', 'Frédéric', 'fdudouit', '44im8', '18 rue de l église', '23120', 'GrandBourg', '2000-08-01'),
+('e49', 'Duncombe', 'Claude', 'cduncombe', 'qf77j', '19 rue de la tour', '23100', 'La souteraine', '1987-10-10'),
+('e5', 'Enault-Pascreau', 'Céline', 'cenault', 'y2qdu', '25 place de la gare', '23200', 'Gueret', '1995-09-01'),
+('e52', 'Eynde', 'Valérie', 'veynde', 'i7sn3', '3 Grand Place', '13015', 'Marseille', '1999-11-01'),
+('f21', 'Finck', 'Jacques', 'jfinck', 'mpb3t', '10 avenue du Prado', '13002', 'Marseille', '2001-11-10'),
+('f39', 'Frémont', 'Fernande', 'ffremont', 'xs5tq', '4 route de la mer', '13012', 'Allauh', '1998-10-01'),
+('f4', 'Gest', 'Alain', 'agest', 'dywvt', '30 avenue de la mer', '13025', 'Berre', '1985-11-01'),
+('cn20', 'napolitano', 'cyril', 'cyril', 'toolsun', '19 rue gayet', '42000', 'Saint Etienne', '2014-09-01'),
+('daf', 'daf', 'daf', 'daf', 'daf', '2 rue du daf', '42100', 'daf', '2014-10-01'),
+('cc20', 'napolitano', 'cyril', 'cyrils', 'tools', '19 rue gayet', '42000', 'Saint Etienne', '2014-09-01'),
+('cd0', 'DAVULCU', 'cihat', 'cdav', '111', '12', '32000', 'strfc', '2014-10-01');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
